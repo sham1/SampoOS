b97045fdc3dbaea892bc736d56cbe50cd0e07530
Jani Juhani Sinervo <jani@sinervo.fi>
2021-03-22 10:04:55 +0200

    Do not use recursive Make

--- build.sh
+++ build.sh
@@ -2,7 +2,5 @@
 
 for arch in x86_64
 do
-    export ARCH="${arch}"
-    . ./arch-flags.sh
-    make -C Kernel
+    make -C Kernel ARCH="${arch}"
 done
---
+++ clean.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+for arch in x86_64
+do
+    make -C Kernel ARCH="${arch}" clean
+done
