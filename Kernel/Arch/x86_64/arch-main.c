3075354534e11927cfb65d9de5a643ad84cff032
Jani Juhani Sinervo <jani@sinervo.fi>
2021-03-23 13:18:07 +0200

    Add ability for architecture-specific initialization

---
+++ Kernel/Arch/x86_64/arch-main.c
@@ -0,0 +1,7 @@
+#include <SampoOS/Kernel/bootinfo.h>
+
+void
+kernel_arch_init(struct sampo_bootinfo *info)
+{
+	(void) info;
+}
--- Kernel/Arch/x86_64/start.asm
+++ Kernel/Arch/x86_64/start.asm
@@ -8,9 +8,19 @@ section .text
 global _start:function (_start.end - _start)
 _start:
 	mov rsp, stack_top
+	mov rbp, rsp
+	; Save the Kickstart bootinfo pointer
+	push rax
+
+	cli
+;extern _init:function
+;	call _init
+
+	pop rdi			; Move bootinfo given by Kickstart to the first argument 
+extern kernel_arch_init:function
+	call kernel_arch_init
 
 	; TODO: Add literally everything else
-	cli
 .hang:
 	hlt
 	jmp .hang
