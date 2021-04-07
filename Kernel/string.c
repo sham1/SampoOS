c6f1fdeff9e5777be3ed15c61aaa5d95343f1f98
Jani Juhani Sinervo <jani@sinervo.fi>
2021-04-07 22:09:23 +0300

    Add some standard string.h implementations for kernel

---
+++ Kernel/string.c
@@ -0,0 +1,115 @@
+#include <string.h>
+
+void *
+memmove(void *dest, const void *src, size_t count)
+{
+	unsigned char *destination = dest;
+	const unsigned char *source = src;
+
+	if (destination < source)
+	{
+		for (size_t i = 0; i < count; ++i)
+		{
+			destination[i] = source[i];
+		}
+	}
+	else
+	{
+		for (size_t i = count; i != 0; --i)
+		{
+			destination[i-1] = source[i-1];
+		}
+	}
+
+	return dest;
+}
+
+void *
+memcpy(void *restrict dest, const void *restrict src, size_t count)
+{
+	unsigned char *d = dest;
+	const unsigned char *s = src;
+	for (size_t i = 0; i < count; ++i)
+	{
+		d[i] = s[i];
+	}
+
+	return dest;
+}
+
+void *
+memset(void *dest, int ch, size_t count)
+{
+	unsigned char *d = dest;
+	unsigned char c = ch;
+	for (size_t i = 0; i < count; ++i)
+	{
+		d[i] = c;
+	}
+
+	return dest;
+}
+
+int
+memcmp(const void *lhs, const void *rhs, size_t count)
+{
+	const unsigned char *a = lhs;
+	const unsigned char *b = rhs;
+
+	for (size_t i = 0; i < count; ++i)
+	{
+		if (a[i] < b[i])
+		{
+			return -1;
+		}
+		else if (a[i] > b[i])
+		{
+			return 1;
+		}
+	}
+
+	return 0;
+}
+
+size_t
+strlen(const char *str)
+{
+	const char *end;
+	for (end = str; *end; ++end);
+	return end - str;
+}
+
+int
+strcmp(const char *a, const char *b)
+{
+        while (*a == *b++)
+	{
+		if (*a++ == '\0')
+		{
+			return 0;
+		}
+	}
+
+	return (*(unsigned char *)a) - (*(unsigned char *)--b);
+}
+
+int
+strncmp(const char *a, const char *b, size_t count)
+{
+	for (size_t i = 0; i < count; ++i)
+	{
+		if (a[i] == b[i])
+		{
+			if (*a == '\0')
+			{
+				break;
+			}
+		}
+		else
+		{
+			return (((unsigned char *)a)[i]) - (((unsigned char *)b)[i]);
+		}
+	}
+
+	return 0;
+}
